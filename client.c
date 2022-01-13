5d828d2d6fde8ee720963cabc3af533ec43606d9
fabiecatinella@gmail.com <fabiecatinella@gmail.com>
2022-01-13 22:46:54 +0000

    Finnishing up.

--- client.c
+++ client.c
@@ -1,8 +1,72 @@
 #include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
+
+void dnsLookup(const char *url, const char *port, struct addrinfo **addresses);
 
 int main() {
-    printf("Hello world");
+    // 1. Set up
+    printf("Initialising...\n");
+
+    // 2. Find tcp server via dns lookup
+    const char *url = "www.catinella.co.uk";
+    const char *port = "5000";
+    struct addrinfo *addresses;
+    dnsLookup(url, port, &addresses);
+
+    // 3. Iteration through possible protocals
+    struct addrinfo *addr;
+    int fd;
+
+    for (addr = addresses; addr != NULL; addr = addr->ai_next) {
+        fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+        if (fd == -1) {
+            perror("Couldn't connect to this socket, trying another");
+            continue;
+        }
+
+        if (connect(fd, addr->ai_addr, addr->ai_addrlen) == -1) {
+            close(fd);
+            continue;
+        }
+        break;
+    }
+    if (addr == NULL) {
+        printf("Couldn't connect to url.\n");
+        exit(0);
+    } else {
+        printf("Successfully connected to server.\n");
+    }
+
+    // 4. Send data to Server
+    char *data = "Hello world!";
+    size_t data_len = sizeof(data);
+    int flags = MSG_NOSIGNAL;
+
+    if (send(fd, data, data_len, flags) == -1) {
+        perror("Failed to send data");
+    } else {
+        printf("Hurray I sent some data!");
+    }
+    close(fd);
+}
+
+void dnsLookup(const char *url, const char *port, struct addrinfo **addresses) {
+    struct addrinfo hints;
+    int i;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = PF_UNSPEC;      // Unspecified protocol
+    hints.ai_socktype = SOCK_STREAM;  // Want a tcp socket
+
+    // Do DNS lookup, addressess is a pointer head to a linked list
+    if ((i = getaddrinfo(url, port, &hints, addresses)) != 0) {
+        printf("Error: Unable to lookup IP address: %s", gai_strerror(i));
+    }
 }
--- server.c
+++ server.c
@@ -1,8 +1,66 @@
 #include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
+
+#define BUFLEN 1500
 
 int main() {
-    printf("Hello world");
+    // 1. Set up
+    printf("Initialising...\n");
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (fd == -1) {
+        perror("Something went wrong with the init:");
+        exit(0);
+    }
+
+    // 2. Binding to ip and port
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,          // IPV4
+        .sin_addr.s_addr = INADDR_ANY,  // Any avail network interface
+        .sin_port = htons(5000)         // Port
+    };
+
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        perror("Failed to bind ip and port");
+    }
+
+    // 3. Start listening
+    int backlog = 10;
+
+    if (listen(fd, backlog) == -1) {
+        perror("Server couldn't listen.");
+    }
+    printf("Server listening...\n");
+
+    // 4. Accept connections
+    while (1) {
+        struct sockaddr_in cliaddr;
+        socklen_t cliaddr_len = sizeof(cliaddr);
+
+        // this is the file descriptor of the client
+        int connfd = accept(fd, (struct sockaddr *)&cliaddr, &cliaddr_len);
+        if (connfd == -1) {
+            perror("Failed to connect with incomming client.");
+        }
+
+        // 5. Read Data from client
+        ssize_t rcount;
+        char buf[BUFLEN];
+        int flags = 0;  // No flags set
+
+        rcount = recv(connfd, buf, BUFLEN, flags);
+        if (rcount == -1) {
+            printf("Failed to read data from client.\n");
+        }
+        buf[BUFLEN - 1] = '\0';
+        printf("%s", buf);
+        close(connfd);
+    }
 }
